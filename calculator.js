429ee4d20322a28fbb5b8a85ac8f04cecae1491f
Haya <h_almodahka@icloud.com>
2021-02-02 18:13:40 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,23 @@
 // Define the numbers and the operation here
+let integer1 = 3;
+let integer2 = 5;
+let operation = "/";
 
 // Write if-statements here
+if (operation === "+") {
+
+    console.log("3 + 5 = " + (integer1+integer2));
+
+} else if (operation === "*"){
+
+    console.log("3 * 5 = " + (integer1*integer2));
+
+} else if (operation === "-") {
+
+
+    console.log("3 - 5 = " + (integer1-integer2));
+
+} else if (operation === "/") {
+
+    console.log("3 / 5 = " + (integer1/integer2));
+}
